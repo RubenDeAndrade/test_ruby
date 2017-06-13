948532846d336d5b7fa41e751d54442f3872c278
papilip <me@papilip.me>
2017-06-13 14:26:42 +0200

    Modify app for view members in organization

--- part_three/config/routes.rb
+++ part_three/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   resources :organizations do
   	resources :members
   end
+
+  root to: 'organizations#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
